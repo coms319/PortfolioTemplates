c559aac87d251d0b07303ad0dc7b22556923b6e2
LucasNeroneRillo <lneronerillo@gmail.com>
2024-10-14 09:58:08 -0500

    Updated about page

---
+++ Portfolio_Templates/team.js
@@ -0,0 +1,28 @@
+fetch('./data.json')
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    populatePage(data);
+})
+.catch(error => {
+    console.log(error);
+})
+
+function populatePage(data) {
+    document.getElementById('team').innerHTML = '';
+    data.teamPage.teamMembers.forEach(member => {
+        document.getElementById('team').innerHTML += `
+            <div class="col">
+                <div class="card h-100">
+                    <img src="${member.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h2 class="h3 mb-0">${member.name}</h2>
+                        <p class="card-text mb-0" style="font-size: larger;">${member.role}</p>
+                        <p class="text-muted">${member.bio}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
